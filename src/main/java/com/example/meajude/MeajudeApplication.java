d8aa201eb94611e9f7cc3fe6543debe61548b827
Gabriel-J-Code <gabriel.franca@dcx.ufpb.br>
2023-09-27 09:26:21 -0300

    spring init

---
+++ src/main/java/com/example/meajude/MeajudeApplication.java
@@ -0,0 +1,13 @@
+package com.example.meajude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeajudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeajudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meajude/MeajudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meajude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeajudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
