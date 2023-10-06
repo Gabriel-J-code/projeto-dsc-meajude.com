30bd1fc1ac3f9c27bcd1efbcca2afbe4904bd66f
Gabriel-J-code <gabriel.franca@dcx.ufpb.br>
2023-10-06 18:20:45 -0300

    User attributes

--- src/main/java/com/example/meajude/entities/User.java
+++ src/main/java/com/example/meajude/entities/User.java
@@ -1,5 +1,80 @@
 package com.example.meajude.entities;
 
 public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String numDoc;
+    private String typeDoc;
+    private boolean active;
+    
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNumDoc() {
+        return numDoc;
+    }
+
+    public void setNumDoc(String numDoc) {
+        this.numDoc = numDoc;
+    }
+
+    public String getTypeDoc() {
+        return typeDoc;
+    }
+
+    public void setTypeDoc(String typeDoc) {
+        this.typeDoc = typeDoc;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
     
 }
