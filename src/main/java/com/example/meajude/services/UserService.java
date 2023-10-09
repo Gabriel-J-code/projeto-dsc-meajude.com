332b08f45c120b3be2c2e4f27bc27e583c67f3d7
Gabriel-J-code <gabriel.franca@dcx.ufpb.br>
2023-10-09 20:35:07 -0300

    User Service

---
+++ src/main/java/com/example/meajude/services/UserService.java
@@ -0,0 +1,7 @@
+package com.example.meajude.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService {
+    UserDetailsService userDetailsService();    
+}
---
+++ src/main/java/com/example/meajude/services/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.meajude.services.impl;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.meajude.repositories.UserRepository;
+import com.example.meajude.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+}
