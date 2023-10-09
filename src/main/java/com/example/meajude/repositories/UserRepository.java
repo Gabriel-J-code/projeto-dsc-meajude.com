c936c352f9a9db9da7336f4bbd5139bbf58796bc
Gabriel-J-code <gabriel.franca@dcx.ufpb.br>
2023-10-09 20:31:24 -0300

    anotation Repository

--- src/main/java/com/example/meajude/repositories/UserRepository.java
+++ src/main/java/com/example/meajude/repositories/UserRepository.java
@@ -3,9 +3,10 @@ package com.example.meajude.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.meajude.entities.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
     Optional<User> findByEmail(String email);
 }
