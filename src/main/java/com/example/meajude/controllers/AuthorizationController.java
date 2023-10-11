b398533096d6b9f462b9dcb8abd757ebc75f2a6f
Gabriel-J-code <gabriel.franca@dcx.ufpb.br>
2023-10-11 00:04:18 -0300

    testing to get user email

--- src/main/java/com/example/meajude/controllers/AuthorizationController.java
+++ src/main/java/com/example/meajude/controllers/AuthorizationController.java
@@ -5,14 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.meajude.services.JwtService;
+
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/resource")
 @RequiredArgsConstructor
 public class AuthorizationController {
+    private final JwtService jwts;
     @GetMapping
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Here is your resource");
+    public ResponseEntity<String> sayHello(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");        
+        String user = jwts.extractUserName(token);
+        return ResponseEntity.ok(user) ;
     }
 }
