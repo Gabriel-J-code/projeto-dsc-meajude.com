811b642d9b2396a6817b2b383edd680021f47019
whendel morais <whendel.morais.9@gmail.com>
2023-10-12 16:24:45 -0300

    feat(DM-1): support user registration. Added User-related resources: service, DTO, controller, and entity. Added Api Exception And User not found Exception.

---
+++ src/main/java/com/example/meajude/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,65 @@
+package com.example.meajude.exceptions;
+
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.example.meajude.exceptions.ApiExceptions.ApiException;
+
+import jakarta.annotation.Nullable;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<Object> handleApiException(ApiException e) {
+        Map<String, Object> body = Map.of(
+            "message", e.getMsg(),
+            "code", e.getCode()
+        );
+        
+        return ResponseEntity.status(e.getCode()).body(body);
+    }
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception e) {
+        Map<String, Object> body = Map.of(
+            "message", e.getMessage(),
+            "code", HttpStatus.INTERNAL_SERVER_ERROR.value()
+        );
+        
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+    }
+
+    @Override
+    @Nullable
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+         Map<String, Object> body = new HashMap<>();
+        body.put("message", "Invalid fields");
+        body.put("code", HttpStatus.BAD_REQUEST.value());
+        
+        List<String> errors = ex.getBindingResult().getAllErrors().stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .collect(Collectors.toList());
+        
+        body.put("errors", errors);
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+}
