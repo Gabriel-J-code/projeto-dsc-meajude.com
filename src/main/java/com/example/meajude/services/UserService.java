7503ef0d0ea22a4a905110e83bf0d76f34cc0b9c
Gabriel-J-code <gabriel.franca@dcx.ufpb.br>
2023-10-16 22:12:25 -0300

    remover unused

--- src/main/java/com/example/meajude/services/UserService.java
+++ src/main/java/com/example/meajude/services/UserService.java
@@ -3,9 +3,6 @@ package com.example.meajude.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.meajude.dtos.RegisterUserDTO;
-import com.example.meajude.dtos.UserRegisteredDTO;
-import com.example.meajude.entities.User;
 import com.example.meajude.repositories.UserDAO;
 
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,20 +14,6 @@ public class UserService {
 
     @Autowired
     private UserDAO userDAO;
-
-    public UserRegisteredDTO registerUser(RegisterUserDTO user) {
-
-        if (userDAO.existsByEmail(user.getEmail())) {
-            throw new RuntimeException("Email already exists");
-        }
-
-        User userEntity = new User(user.getName(), user.getEmail(), user.getPassword(), user.getPhone(),
-            user.getDocumentNumber(), user.getDocumentType());
-
-        userDAO.save(userEntity);
-
-        return UserRegisteredDTO.fromEntity(userEntity);
-    }
 
     public UserDetailsService userDetailsService() {
         return new UserDetailsService() {
