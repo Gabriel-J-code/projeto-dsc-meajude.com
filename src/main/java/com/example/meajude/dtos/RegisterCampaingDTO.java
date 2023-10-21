47d366b29bf98ed6080ac8be5632acf2c6de2c84
Gabriel-J-code <gabriel.franca@dcx.ufpb.br>
2023-10-21 16:41:10 -0300

    Validation Campaing

--- src/main/java/com/example/meajude/dtos/RegisterCampaingDTO.java
+++ src/main/java/com/example/meajude/dtos/RegisterCampaingDTO.java
@@ -4,70 +4,75 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import com.example.meajude.entities.Campaign;
+import com.example.meajude.entities.User;
+import com.example.meajude.enums.State;
 
 public class RegisterCampaingDTO {
     private String title;
     private String smallTitle;
     private String description;
     private double goal;
-    private String startDate;
     private String endDate;
 
     public RegisterCampaingDTO() {
     }
 
-    public RegisterCampaingDTO(String title, String smallTitle, String description, double goal, LocalDate startDate,
-            LocalDate endDate) {
+    public RegisterCampaingDTO(String title, String smallTitle, String description, double goal,
+            String endDate) {
         this.title = title;
         this.smallTitle = smallTitle;
         this.description = description;
         this.goal = goal;
-        this.startDate = startDate.toString();
-        this.endDate = endDate.toString();
+        this.endDate = endDate;
     }
 
-    public Campaign toCampaing(){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return new Campaign(title, smallTitle, description, goal, LocalDate.parse(startDate,dtf), LocalDate.parse(endDate, dtf));
+    public Campaign toCampaing(User user, State state) {
+        return new Campaign(title, smallTitle, description, goal, LocalDate.now(), getEndDateFormat(), user, state);
     }
 
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getSmallTitle() {
         return smallTitle;
     }
+
     public void setSmallTitle(String smallTitle) {
         this.smallTitle = smallTitle;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public double getGoal() {
         return goal;
     }
+
     public void setGoal(double goal) {
         this.goal = goal;
     }
-    public String getStartDate() {
-        return startDate;
-    }
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
+
     public String getEndDate() {
         return endDate;
     }
+
+    public LocalDate getEndDateFormat(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return LocalDate.parse(endDate, dtf);
+    }
+
     public void setEndDate(String endDate) {
         this.endDate = endDate;
     }
-
-    
 
 }
--- src/main/java/com/example/meajude/services/CampaignService.java
+++ src/main/java/com/example/meajude/services/CampaignService.java
@@ -1,5 +1,7 @@
 package com.example.meajude.services;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +10,9 @@ import com.example.meajude.dtos.RegisterCampaingDTO;
 import com.example.meajude.entities.Campaign;
 import com.example.meajude.entities.User;
 import com.example.meajude.enums.State;
+import com.example.meajude.exceptions.ApiExceptions.InvalidFieldCampaingException;
 import com.example.meajude.repositories.CampaignDAO;
 
-import jakarta.servlet.http.HttpServletRequest;
 
 @Service
 public class CampaignService {
@@ -21,11 +23,35 @@ public class CampaignService {
     private UserService userService;
 
     public CampaignDTO registerCampaing(String authHeader, RegisterCampaingDTO rCDto) {
+        validationCampaing(rCDto);
         User user = userService.getUserToRequest(authHeader);
-        Campaign campaign = rCDto.toCampaing();
-        campaign.setUser(user);
-        campaign.setState(State.ACTIVE);              
+        Campaign campaign = rCDto.toCampaing(user, State.ACTIVE);            
         return new CampaignDTO(campaignDAO.save(campaign));
+    }
+
+    public void validationCampaing(RegisterCampaingDTO rcdto){ 
+        validationStringField(rcdto.getTitle(), "Title is required");  
+        validationStringField(rcdto.getSmallTitle(), "Small Title is required"); 
+        if(rcdto.getSmallTitle().length() > 100){
+            throw new InvalidFieldCampaingException("Small title cannot be longer than 100 characters");
+        }
+        validationStringField(rcdto.getDescription(), "Description is required"); 
+        if(rcdto.getDescription().length() > 1000){
+            throw new InvalidFieldCampaingException("Description cannot be longer than 1000 characters");
+        }
+        if((rcdto.getGoal() <= 0) ){
+            throw new InvalidFieldCampaingException("Goal must be greater than zero " + rcdto.getGoal());
+        }
+        validationStringField(rcdto.getEndDate(), "End Date is required");
+        if(rcdto.getEndDateFormat().isBefore(LocalDate.now()) || rcdto.getEndDateFormat().isEqual(LocalDate.now())){
+            throw new InvalidFieldCampaingException("End Date must be in the future");
+        }
+    }
+
+    public void validationStringField(String field, String messageException){
+        if(field.isBlank()){
+            throw new InvalidFieldCampaingException(messageException);
+        }
     }
 
 }
